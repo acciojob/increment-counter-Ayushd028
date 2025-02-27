de162641ae64d996591ea1be4515c5fe474d0617
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-02-27 19:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let counterValue = 0;
+        document.getElementById("incrementBtn").addEventListener("click", function() {
+            alert("Un-incremented value: " + counterValue);
+            counterValue++;
+            document.getElementById("counter").textContent = counterValue;
+        });
